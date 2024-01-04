95509bd1c0e083b6874c38e24232ee56ad5fb9e1
ibsalam <asio4edu@gmail.com>
2024-01-04 13:21:51 -0800

    imports functions from the file, does math and print

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+a = 10
+b = 5
+from calculator_1 import add, sub, mul, div
+result_add = add(a, b)
+result_sub = sub(a, b)
+result_mul = mul(a, b)
+result_div = div(a, b)
+print("10 + 5 =", result_add)
+print("10 - 5 =", result_sub)
+print("10 * 5 =", result_mul)
+print("10 / 5 =", result_div)
