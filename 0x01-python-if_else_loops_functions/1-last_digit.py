6062fb1a305d953bd96a1d6433a1a3ca2f932972
ibsalam <asio4edu@gmail.com>
2024-01-03 19:12:08 -0800

    asign random value to variable number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastdigit = abs(number) % 10
+
+last_digit = abs(number) % 10
+
+thestring = "last digit of {} is {}".format(number, last_digit)
+
 if number < 0:
-    lastdigit = -(lastdigit)
-thestring = "lastdigit of {} is {}".format(number, lastdigit)
-if lastdigit > 5:
+    last_digit *= -1
+if last_digit > 5:
     print(f"{thestring} and is greater than 5")
-elif lastdigit == 0:
+elif last_digit == 0:
     print(f"{thestring} and is 0")
-elif lastdigit < 6:
+else:
     print(f"{thestring} and is less than 6 and not 0")
