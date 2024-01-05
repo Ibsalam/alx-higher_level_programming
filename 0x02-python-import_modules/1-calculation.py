d03755e68f8dc3f47098a398efc314c24f697371
ibsalam <asio4edu@gmail.com>
2024-01-04 18:46:50 -0800

    program that imports functions from the file, does some maths and prints result

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    from calculator_1 import add, sub, mul, div
     result_add = add(a, b)
     result_sub = sub(a, b)
     result_mul = mul(a, b)
