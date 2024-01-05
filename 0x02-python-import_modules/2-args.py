6c8981f672338abd95566ea8c363415b8a9cb722
ibsalam <asio4edu@gmail.com>
2024-01-04 19:11:18 -0800

    make a script dynamic

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    num_args = len(sys.argv) - 1
+if num_args == 0:
+    print("0 argument(s):")
+    print(".")
+else:
+    print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
+    
+    for i in range(1, num_args + 1):
+        print(f"{i}: {sys.argv[i]}")
