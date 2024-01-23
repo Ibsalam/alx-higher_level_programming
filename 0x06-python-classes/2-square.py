20e4a9c130cc07acffbd1c1706a28bb10d8e71bd
ibsalam <asio4edu@gmail.com>
2024-01-23 12:21:51 -0800

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square (default is 0).
+        """
+        self.size = size
