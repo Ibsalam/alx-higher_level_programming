31f039ac7ba6b0b118660b2c3bf5a78f863a3036
ibsalam <asio4edu@gmail.com>
2024-04-08 21:54:09 -0700

    javascript warmup

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-
-const arg2 = process.argv[2];
-const parsedNumber = parentInt{arg2};
-
-if (isNaN(parentNumber)) {
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Not a number');
 } else {
-  console.log('My number: ${parentNumber}');
+  console.log('My number:', parseInt(process.argv[2]));
 }
