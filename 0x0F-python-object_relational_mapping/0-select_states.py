8e1c469bc92edb39a2a5feb41a30620c8a5e64a2
ibsalam <asio4edu@gmail.com>
2024-04-20 14:07:13 -0700

    ORM task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,11 +2,11 @@
 """Lists states"""
 
 import MySQLdb
-from sys import argv
+from sys
 
 if __name__ == "__main__":
-    dbconn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+    dbconn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
     curr = dbconn.cursor()
     curr.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_rows = curr.fetchall()
