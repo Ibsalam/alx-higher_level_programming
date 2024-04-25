6f6095e89dc7628563dec3d7ee604e5fd643750b
ibsalam <asio4edu@gmail.com>
2024-04-25 12:52:24 -0700

    pyhton network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a GET request to the URL and store the response body size
-response=$(curl -sI "$1" | grep -i "Content-Length" | cut -d " " -f2 | tr -d '\r\n')
-
-# Display the size of the response body in bytes
-echo "Size of the response body: $response bytes"
-
+# takes in a URL, sends a request to that URL, displays the size of the body of the response
+curl -sI GET "$1" | grep -i "Content-Length" | cut -d " " -f2
